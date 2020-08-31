97ef8672337e13207b788822d1547347f1d02e51
schneems <richard.schneeman+foo@gmail.com>
2020-08-31 14:02:16 -0500

    Allow pipe commands to start with $
    Pipe commands can take two forms:
    
    ```
    | tail
    ```
    
    and:
    
    ```
    | $ tail
    ```
    
    So that fully qualified commands can be used. There was a bug that commands that started with $ were not being used.
    
    Prior to this the parser was expecting a `command` which looks like this:
    
    ```
        # :::>> $ cat foo.rb
        rule(:command) {
          (
            start_command >>
            visability.as(:cmd_visability) >> spaces? >>
            method_call.as(:cmd_method_call) >> newline.maybe #>> match(/\z/)
          ).as(:command)
        }
    ```
    
    Which would never be matched. So we were accidentally relying on the failure rescue mode from the parser to give us the behavior for `| tail`.
    
    With this change any commands should be allowed such as `| $ tail`.

--- test/rundoc/code_commands/pipe_test.rb
+++ test/rundoc/code_commands/pipe_test.rb
@@ -1,12 +1,22 @@
 require 'test_helper'
 
 class PipeTest < Minitest::Test
-
   def test_pipe
     pipe_cmd = "tail -n 2"
     cmd      = "ls"
     out      = `#{cmd}`
     pipe     = Rundoc::CodeCommand::Pipe.new(pipe_cmd)
+    actual   = pipe.call(commands: [{command: cmd, output: out}])
+
+    expected = `#{cmd} | #{pipe_cmd}`
+    assert_equal expected, actual
+  end
+
+  def test_moar_pipe_with_dollar
+    pipe_cmd = "tail -n 2"
+    cmd      = "ls"
+    out      = `#{cmd}`
+    pipe     = Rundoc::CodeCommand::Pipe.new("$ #{pipe_cmd}")
     actual   = pipe.call(commands: [{command: cmd, output: out}])
 
     expected = `#{cmd} | #{pipe_cmd}`
