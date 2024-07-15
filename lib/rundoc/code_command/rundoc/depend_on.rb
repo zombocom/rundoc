ee590dc44dfca83f5636133308a6de91cb7c8599
Schneems <richard.schneeman+foo@gmail.com>
2024-07-15 15:42:01 -0500

    Increase test coverage of deprecated function

--- lib/rundoc/code_command/rundoc/depend_on.rb
+++ lib/rundoc/code_command/rundoc/depend_on.rb
@@ -4,24 +4,7 @@ class ::Rundoc::CodeCommand
       # Pass in the relative path of another rundoc document in order to
       # run all of it's commands (but not to )
       def initialize(path)
-        raise "Path must be relative (i.e. start with `.` or `..`. #{path.inspect} does not" unless path.start_with?(".")
-        @path = Pathname.new(path)
-      end
-
-      def to_md(env = {})
-        ""
-      end
-
-      def call(env = {})
         raise "rundoc.depend_on has been removed, use `:::-- rundoc.require` instead"
-      end
-
-      def hidden?
-        true
-      end
-
-      def not_hidden?
-        !hidden?
       end
     end
   end
