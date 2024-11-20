e883245c90f2221f73ad39ac6592bbff9b4dfd41
Schneems <richard.schneeman+foo@gmail.com>
2024-11-20 17:21:29 -0600

    Write failing test

--- test/integration/failure_test.rb
+++ test/integration/failure_test.rb
@@ -1,7 +1,58 @@
 require "test_helper"
 
 class IntegrationFailureTest < Minitest::Test
-  def test_writes_to_dir_on_failure
+  def test_writes_to_dir_on_failure_two_block
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        dir = Pathname(dir)
+
+        source_path = dir.join("RUNDOC.md")
+        source_path.write <<~EOF
+          ```
+          :::>> $ mkdir one
+          :::>> $ touch one/rofl.txt
+          ```
+
+          ```
+          :::>> $ mkdir two
+          :::>> $ touch two/rofl.txt
+          :::>> $ touch does/not/exist.txt
+          ```
+        EOF
+
+        io = StringIO.new
+
+        error = nil
+        begin
+          Rundoc::CLI.new(
+            io: io,
+            source_path: source_path,
+            on_success_dir: dir.join(SUCCESS_DIRNAME)
+          ).call
+        rescue => e
+          error = e
+        end
+
+        assert error
+        assert_includes error.message, "exited with non zero status"
+
+        refute dir.join(SUCCESS_DIRNAME).join("two").exist?
+        refute dir.join(SUCCESS_DIRNAME).join("two").join("rofl.txt").exist?
+
+        assert dir.join(FAILURE_DIRNAME).join("two").exist?
+        assert dir.join(FAILURE_DIRNAME).join("two").join("rofl.txt").exist?
+
+        doc = dir.join(FAILURE_DIRNAME).join("RUNDOC_FAILED.md").read
+        assert_includes doc, "$ mkdir one"
+        assert_includes doc, "$ touch one/rofl.txt"
+
+        assert_includes doc, "$ mkdir two"
+        assert_includes doc, "$ touch two/rofl.txt"
+      end
+    end
+  end
+
+  def test_writes_to_dir_on_failure_one_block
     Dir.mktmpdir do |dir|
       Dir.chdir(dir) do
         dir = Pathname(dir)
