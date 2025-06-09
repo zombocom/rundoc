9f78e23813435831e4369a4ec2768d0c7f9626fe
Schneems <richard.schneeman+foo@gmail.com>
2025-06-09 15:26:04 -0500

    Test background task prints contents on abort

--- test/integration/background_stdin_test.rb
+++ test/integration/background_stdin_test.rb
@@ -41,6 +41,43 @@ class BackgroundStdinTest < Minitest::Test
     end
   end
 
+  def test_print_output_on_exit
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        dir = Pathname(dir)
+        script = dir.join("script.rb")
+        script.write loop_script
+
+        source_path = dir.join("RUNDOC.md")
+        source_path.write <<~EOF
+          ```
+          :::-- background.start("ruby #{script}",
+            name: "background_ungraceful_exit",
+            wait: ">",
+            timeout: 15
+          )
+          :::-- background.stdin_write("hello", name: "background_ungraceful_exit", wait: "hello")
+          ```
+        EOF
+
+        io = StringIO.new
+        Rundoc::CLI.new(
+          io: io,
+          source_path: source_path,
+          on_success_dir: dir.join(SUCCESS_DIRNAME)
+        ).call
+
+        assert_include(actual: io.string, include_str: "Warning background task is still running, cleaning up: name: script")
+        assert_include(actual: io.string, include_str: "Log contents for `/usr/bin/env bash -c")
+        assert_include(actual: io.string, include_str: "You said: hello")
+      end
+    end
+  end
+
+  def assert_include(actual: , include_str:)
+    assert actual.include?(include_str), "Expected to find `#{include_str}` in output, but did not. Output:\n#{actual}"
+  end
+
   def loop_script
     <<~'EOF'
       $stdout.sync = true
