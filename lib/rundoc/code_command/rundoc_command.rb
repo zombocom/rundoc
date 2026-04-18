38e820d2457f4172b888d6b42a9ba45e1172cde4
Schneems <richard.schneeman+foo@gmail.com>
2026-04-18 16:58:51 -0500

    Refactor RundocCommand to Args + Runner pattern

--- lib/rundoc/code_command/rundoc_command.rb
+++ lib/rundoc/code_command/rundoc_command.rb
@@ -1,8 +1,16 @@
 module ::Rundoc
   class CodeCommand
-    class ::RundocCommand < ::Rundoc::CodeCommand
+    class RundocCommandArgs
+      attr_reader :contents
+
       def initialize(contents = "")
         @contents = contents
+      end
+    end
+
+    class RundocCommandRunner < ::Rundoc::CodeCommand
+      def initialize(user_args:)
+        @contents = user_args.contents
       end
 
       def to_md(env = {})
@@ -18,8 +26,8 @@ module ::Rundoc
   end
 end
 
-Rundoc.register_code_command(:rundoc, RundocCommand)
-Rundoc.register_code_command(:"rundoc.configure", RundocCommand)
+Rundoc.register_code_command(:rundoc, Rundoc::CodeCommand::RundocCommandArgs, runner: Rundoc::CodeCommand::RundocCommandRunner)
+Rundoc.register_code_command(:"rundoc.configure", Rundoc::CodeCommand::RundocCommandArgs, runner: Rundoc::CodeCommand::RundocCommandRunner)
 
 require "rundoc/code_command/rundoc/depend_on"
 require "rundoc/code_command/rundoc/require"
