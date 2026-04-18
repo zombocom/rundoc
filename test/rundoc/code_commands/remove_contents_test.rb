bf3e81fb0da993c33ea812393aa562a0ffb9f007
Schneems <richard.schneeman+foo@gmail.com>
2026-04-18 16:52:57 -0500

    Refactor FileCommand::Remove to Args + Runner pattern

--- test/rundoc/code_commands/remove_contents_test.rb
+++ test/rundoc/code_commands/remove_contents_test.rb
@@ -21,7 +21,7 @@ class RemoveContentsTest < Minitest::Test
 
         assert_match(/sqlite3/, File.read(@file))
 
-        cc = Rundoc::CodeCommand::FileCommand::Remove.new(@file)
+        cc = Rundoc::CodeCommand::FileCommand::RemoveRunner.new(user_args: Rundoc::CodeCommand::FileCommand::RemoveArgs.new(@file))
         cc << "gem 'sqlite3'"
         cc.call
 
